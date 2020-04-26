89670db5c59409af3d1cb030c6aa84f91162dbd9
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2020-04-26 05:28:11 +0200

    Allow user to register and send JWT data as a response

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Passport\Client;
+
+class UserController extends Controller
+{
+    
+    /*
+        Create new user
+    */
+    public function register(Request $request){
+
+        // Validate the request
+        $results = Validator::make($request->all(), [
+            "email" => "bail|required|email|unique:users,email",
+            "name" => "bail|required|string|min:3|max:15",
+            "password" => "bail|required|min:6|confirmed"
+        ]);
+
+        if($results->fails()){
+            return response()->json([
+                "errors"    => $results->errors()
+            ], 401);
+        }
+
+        // Create new user
+        $user = new User([
+            "email" => $request->email,
+            "name" => $request->name,
+            "password" => Hash::make($request->password)
+        ]);
+
+        $user->save();
+
+
+        // Make internal request to send the JWT to the user
+        
+        // Get client id and secret
+        $client = Client::where("password_client", 1)->first();
+
+
+        // Add data to the request
+        $request->request->add([
+            'grant_type'    => 'password',
+            'client_id'     => $client->id,
+            'client_secret' => $client->secret,
+            'username'      => $request->email,
+            'password'      => $request->password,
+            'scope'         => ''
+        ]);
+
+
+        $token = Request::create(
+            'oauth/token',
+            'POST'
+        );
+    
+        return \Route::dispatch($token);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+// Register new user
+Route::post("/oauth/register", "UserController@register");
