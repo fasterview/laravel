4ecc976ceb536a2a0d4faa2bc6643ed83d77d24c
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2020-04-27 08:05:31 +0200

    Added sluggable to Org model
    Installed and configed cviebrock/eloquent-sluggable package
    Add elquent-sluggable to Org model

--- app/Org.php
+++ app/Org.php
@@ -3,13 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Org extends Model
 {
+
+    use Sluggable;
+
     protected   $table = "orgs",
                 $primaryKey = "id";
     
     
+
+    
+    // Sluggable
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+        
+
     public function user(){
         return $this->belongsTo("App\User", "user_id", "id");
     }
