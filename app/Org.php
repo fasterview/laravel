0b7869073aa96b730d95ae591019162f66fc3313
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2020-05-31 11:32:56 +0200

    Added owner property to Org model

--- app/Org.php
+++ app/Org.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Support\Facades\Auth;
 
 class Org extends Model
 {
@@ -13,7 +14,7 @@ class Org extends Model
     protected   $table = "orgs",
                 $primaryKey = "id";
     
-    
+    protected $appends = ["is_owner"];
 
     
     // Sluggable
@@ -30,6 +31,11 @@ class Org extends Model
 
     public function user(){
         return $this->belongsTo("App\User", "user_id", "id");
+    }
+
+    // Is owner attribute
+    public function getIsOwnerAttribute(){
+        return $this->attributes['user_id'] == Auth::user()->id;
     }
 
     // Relationship with intervies
